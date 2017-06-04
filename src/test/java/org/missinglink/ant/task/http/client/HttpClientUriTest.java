d09d530cb894919685a44a585246e25a1b51d1ab
twogee <g.grigelionis@gmail.com>
2017-06-04 18:07:05 +0200

    Added Ivy ant test target (thanks to nlalevee)

--- src/test/java/org/missinglink/ant/task/http/client/HttpClientUriTest.java
+++ src/test/java/org/missinglink/ant/task/http/client/HttpClientUriTest.java
@@ -99,8 +99,8 @@ public class HttpClientUriTest {
     client = HttpClient.uri("http://host/context/longer/file.html?qp=value with spaces").toHttpClient();
     Assert.assertEquals("http://host/context/longer/file.html?qp=value+with+spaces", client.getUri());
     client = HttpClient.uri("http://host/context/longer/?qp=value with spaces&qp2=value2").toHttpClient();
-    Assert.assertEquals("http://host/context/longer/?qp=value+with+spaces&qp2=value2", client.getUri());
+    Assert.assertEquals("http://host/context/longer/?qp2=value2&qp=value+with+spaces", client.getUri());
     client = HttpClient.uri("http://host/context/longer/file.html?qp=value with spaces&qp2=value2").toHttpClient();
-    Assert.assertEquals("http://host/context/longer/file.html?qp=value+with+spaces&qp2=value2", client.getUri());
+    Assert.assertEquals("http://host/context/longer/file.html?qp2=value2&qp=value+with+spaces", client.getUri());
   }
 }
